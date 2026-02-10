b7ddf8a0c7e19cca24cd08a256a7a2da5b1e16b0
Oleg <arefevpro@yandex.ru>
2026-02-10 23:11:27 +0300

    Adjust Photo Grid for Mobile

--- app/components/station/modules/StationPhotoModule.tsx
+++ app/components/station/modules/StationPhotoModule.tsx
@@ -337,7 +337,7 @@ export default function StationPhotoModule() {
               </div>
             ) : (
               <>
-                <div className="grid grid-cols-2 gap-2">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
                   {activeAlbum.photos.map((photo) => {
                     return (
                       <article
